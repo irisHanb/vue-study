0b57688dddd59e7b2e40ad8ebff3b86e9640ed2a
mz-hanb <hanb@mz.co.kr>
2019-03-06 17:59:39 +0900

    save list to localStorage
    localStorage 에 데이타 저장하기기능 구현

--- todo-v2/src/components/todo/Todo.vue
+++ todo-v2/src/components/todo/Todo.vue
@@ -21,14 +21,24 @@ export default {
   created() {
     eventBus.$on("addTodo", this.addTodo);
     eventBus.$on("removeTodo", this.removeTodo);
+
+    this.list = JSON.parse(localStorage.list) || [];
+    this.cnt = localStorage.cnt || 0;
   },
   methods: {
     addTodo(todoText) {
       this.cnt++;
       this.list.push({ text: todoText, id: this.cnt });
+      this.saveList();
     },
     removeTodo(id) {
       this.list = this.list.filter(ele => id != ele.id);
+      this.saveList();
+    },
+    saveList() {
+      localStorage.list = JSON.stringify(this.list);
+      localStorage.cnt = this.cnt;
+      // console.log("addTodo>", localStorage.list);
     }
   }
 };
