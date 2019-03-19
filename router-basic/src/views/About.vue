8d47c6e965bc835f0a069b7036b62951ab3c52e8
mz-hanb <hanb@mz.co.kr>
2019-03-19 17:32:24 +0900

    add router-basic project

---
+++ router-basic/src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h1>click</h1>
+    <router-link :to="{name: 'home'}">
+      <h1>click</h1>
+    </router-link>
+  </div>
+</template>
